e26dc984d994b0c481b9d7c8a8d3e961485944ce
波卡Poka <j100002ben@gmail.com>
2014-07-19 03:03:45 +0800

    Add P001-P030 and S001-S015 Radio.

--- js/default.js
+++ js/default.js
@@ -15,5 +15,33 @@
 		$.storage.set('item_returntime_'+tmp,'');
 		count_items+=1;
 	}
+	header='P';
+	for(var i=1;i<=30;i++){
+		var tmp=''+i;
+		if(i<10)tmp='0'+tmp;
+		if(i<100)tmp='0'+tmp;
+		tmp=header+tmp;
+		
+		$.storage.set('item_list_'+count_items,tmp);
+		$.storage.set('item_name_'+tmp,'無線電');
+		$.storage.set('item_borrow_'+tmp,'');
+		$.storage.set('item_borrowtime_'+tmp,'');
+		$.storage.set('item_returntime_'+tmp,'');
+		count_items+=1;
+	}
+	header='S';
+	for(var i=1;i<=15;i++){
+		var tmp=''+i;
+		if(i<10)tmp='0'+tmp;
+		if(i<100)tmp='0'+tmp;
+		tmp=header+tmp;
+		
+		$.storage.set('item_list_'+count_items,tmp);
+		$.storage.set('item_name_'+tmp,'無線電');
+		$.storage.set('item_borrow_'+tmp,'');
+		$.storage.set('item_borrowtime_'+tmp,'');
+		$.storage.set('item_returntime_'+tmp,'');
+		count_items+=1;
+	}
 	$.storage.set('item_count',count_items);
 }
